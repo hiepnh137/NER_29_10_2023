cf556f098b424a19d5424866c3c2987c78b98e38
hiepnh137 <hiepnh1307@gmail.com>
2023-10-29 23:01:46 +0700

    committed

--- model.py
+++ model.py
@@ -87,16 +87,16 @@ class NERClassifier(nn.Module):
         """Performs forward pass of the module."""
         # Get token embeddings for each word in a sequence
         x = self.embedding_layer(x) * math.sqrt(self.d_model)
-        print('x: ', x.shape)
+        # print('x: ', x.shape)
         # Map input tokens to the transformer embedding dim
         # x = self.entry_mapping(x)
         # x = F.leaky_relu(x)
         # Leverage the self-attention mechanism on the input sequence
-        print('padding_mask: ', padding_mask.shape)
-        padding_mask = padding_mask.permute(1,0)
+        # print('padding_mask: ', padding_mask.shape)
+        # padding_mask = padding_mask.permute(1,0)
         x = x.permute(1, 0, 2)
         x = self.positional_encodings(x)
-        print('x2: ', x.shape)
+        # print('x2: ', x.shape)
         x = self.transformer_encoder(x, src_key_padding_mask=padding_mask)
         x = x.permute(1, 0, 2)
 
